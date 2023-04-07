ec48648e5e99e39eb9bcbe3c2576295afdcd125b
BloodMagicLord <313353@niuitmo.com>
2023-04-07 14:10:06 +0300

    solution added

--- solution.py
+++ solution.py
@@ -1,11 +1,8 @@
-import pandas as pd
 import numpy as np
 
+chat_id = 632165934  # Ваш chat ID, не меняйте название переменной
+TIME_INTERVAL = 4
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    return np.mean(x) * (2 / TIME_INTERVAL ** 2)
